98e4770b6c344a8883564f09c50f04c3817b31c7
JonathanRubiero <rubiero.jonathan@gmail.com>
2018-07-02 15:12:26 +0200

    Implemented media deletion and removed button disabling

--- assets/js/vision.js
+++ assets/js/vision.js
@@ -21,11 +21,14 @@ function fileListRefresh(wsmessage) {
 	})
 };
 
-$(document).ready(function () {
+/*$(document).ready(function () {
 	$('input:file').on("change", function() {
 	    $('input:submit').prop('disabled', !$(this).val()); 
 	});
-});
+	$('#myFile').on("change", function() {
+		$('input:submit').prop('disabled', !$(this).val()); 
+	});
+});*/
 
 $(document).ready(function () {
     $("#btnSubmit").click(function (event) {
@@ -40,7 +43,7 @@ $(document).ready(function () {
         var data = new FormData(form);
 
 		// disabled the submit button
-        $("#btnSubmit").prop("disabled", true);
+        //$("#btnSubmit").prop("disabled", true);
 
         $.ajax({
             type: "POST",
@@ -53,14 +56,45 @@ $(document).ready(function () {
             timeout: 600000,
             success: function (data) {
                 console.log("SUCCESS : ", data);
-                $("#btnSubmit").prop("disabled", true); 
+                //$("#btnSubmit").prop("disabled", true); 
                 $("#file").val("");
             },
             error: function (e) {
                 console.log("ERROR : ", e);
-                $("#btnSubmit").prop("disabled", true);
+                //$("#btnSubmit").prop("disabled", true);
                 $("#file").val("");
             }
+        });
+    });
+    
+    $("#deleteBtnSubmit").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+		// disabled the submit button
+        //$("#deleteBtnSubmit").prop("disabled", true);
+
+        $("#myFiles option:selected").each(function() {
+        	console.log($(this).text());
+	        $.ajax({
+	            type: "GET",
+	            //enctype: 'multipart/form-data',
+	            url: "http://localhost:10000/api/files/delete/" + $(this).text(),
+	            data: {},
+	            processData: false,
+	            //dataType: "jsonp",
+	            cache: false,
+	            timeout: 600000,
+	            success: function (data) {
+	                console.log("SUCCESS : ", data);
+	                //$("#deleteBtnSubmit").prop("disabled", true); 
+	            },
+	            error: function (e) {
+	                console.log("ERROR : ", e);
+	                //$("#deleteBtnSubmit").prop("disabled", true);
+	            }
+	        });
         });
     });
 });
