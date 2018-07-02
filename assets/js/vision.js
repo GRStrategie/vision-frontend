a84623966fa7c8ea97f46371eec6aa5418505bd9
JonathanRubiero <rubiero.jonathan@gmail.com>
2018-07-02 16:11:47 +0200

    Enabled back button behaviour

--- assets/js/vision.js
+++ assets/js/vision.js
@@ -21,14 +21,14 @@ function fileListRefresh(wsmessage) {
 	})
 };
 
-/*$(document).ready(function () {
-	$('input:file').on("change", function() {
-	    $('input:submit').prop('disabled', !$(this).val()); 
+$(document).ready(function () {
+	$("input:file").on("change", function() {
+	    $("#btnSubmit").prop('disabled', !$(this).val()); 
 	});
-	$('#myFile').on("change", function() {
-		$('input:submit').prop('disabled', !$(this).val()); 
+	$("#myFiles").on("change", function() {
+		$("#deleteBtnSubmit").prop("disabled", !$(this).val());
 	});
-});*/
+});
 
 $(document).ready(function () {
     $("#btnSubmit").click(function (event) {
@@ -42,8 +42,8 @@ $(document).ready(function () {
 		// Create an FormData object 
         var data = new FormData(form);
 
-		// disabled the submit button
-        //$("#btnSubmit").prop("disabled", true);
+		// disabled the submit button while uploading
+        $("#btnSubmit").prop("disabled", true);
 
         $.ajax({
             type: "POST",
@@ -53,48 +53,31 @@ $(document).ready(function () {
             processData: false,
             contentType: false,
             cache: false,
-            timeout: 600000,
+            timeout: 6000000,
             success: function (data) {
-                console.log("SUCCESS : ", data);
-                //$("#btnSubmit").prop("disabled", true); 
-                $("#file").val("");
-            },
+                console.log("SUCCESS : ", data);            },
             error: function (e) {
-                console.log("ERROR : ", e);
-                //$("#btnSubmit").prop("disabled", true);
+                console.log("ERROR : ", e);            
+            },
+            complete: function() {
+            	$("#btnSubmit").prop("disabled", true);
                 $("#file").val("");
             }
         });
     });
     
     $("#deleteBtnSubmit").click(function (event) {
-
-        //stop submit the form, we will post it manually.
         event.preventDefault();
 
-		// disabled the submit button
-        //$("#deleteBtnSubmit").prop("disabled", true);
-
-        $("#myFiles option:selected").each(function() {
-        	console.log($(this).text());
-	        $.ajax({
-	            type: "GET",
-	            //enctype: 'multipart/form-data',
-	            url: "http://localhost:10000/api/files/delete/" + $(this).text(),
-	            data: {},
-	            processData: false,
-	            //dataType: "jsonp",
-	            cache: false,
-	            timeout: 600000,
-	            success: function (data) {
+	    $("#myFiles option:selected").each(function() {
+	    	$.getJSON("http://localhost:10000/api/files/delete/" + $(this).text(), function() {
+	    		}).success(function (data) {
 	                console.log("SUCCESS : ", data);
-	                //$("#deleteBtnSubmit").prop("disabled", true); 
-	            },
-	            error: function (e) {
-	                console.log("ERROR : ", e);
-	                //$("#deleteBtnSubmit").prop("disabled", true);
-	            }
-	        });
+	            }).error(function (e) {
+	                console.log("ERROR : ", e);	                
+	            }).always(function() {
+	            	$("#deleteBtnSubmit").prop("disabled", true);
+            });
         });
     });
 });
